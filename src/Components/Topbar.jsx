f8e5acaaa90c644be87e4879b967f4cb582fc421
Kulsum29 <61190514+Kulsum29@users.noreply.github.com>
2023-10-07 17:31:56 +0530

    Topbar design changes at breakpoint 768px

--- src/Components/Topbar.jsx
+++ src/Components/Topbar.jsx
@@ -1,14 +1,23 @@
+import React from 'react';
 import { NavLink } from "react-router-dom";
 import styles from "./Topbar.module.css";
 import JoinUs from "../ui/Dropdown";
-function Topbar() {
+export default class Topbar extends React.Component {
+  state={clicked:false}
+  handleClick=()=>{
+    this.setState({clicked: !this.state.clicked})
+  }
+  render(){
+
   return (
     <header className={styles.header}>
       <NavLink to="/" className={styles.home}>
         <img src="/logo.png" alt="" />
       </NavLink>
-
-      <ul>
+      <div className={styles.menuicon} onClick={this.handleClick} >
+        <i className={this.state.clicked?"fas fa-times":"fas fa-bars"}></i>
+      </div>
+      <ul className={this.state.clicked?'':styles.hiddenmenu}>
         <NavLink to="/about" className={styles.about}>
           <li>About us</li>
         </NavLink>
@@ -25,6 +34,6 @@ function Topbar() {
       </ul>
     </header>
   );
+  }
 }
 
-export default Topbar;
