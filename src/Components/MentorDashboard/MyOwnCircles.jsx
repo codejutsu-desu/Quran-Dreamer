debd9d62df8e03f7904107ad0d349ad5301af9ca
basit3407 <basit3407@gmail.com>
2023-11-06 15:00:56 +0500

    restored previous logic of brother shimanto

--- src/Components/MentorDashboard/MyOwnCircles.jsx
+++ src/Components/MentorDashboard/MyOwnCircles.jsx
@@ -1,27 +1,27 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchCreatedCircle } from "../../actions"; // Import your action
+import { fetchStudyCircles } from "../../actions"; // Import your action
 import MyOwnCircle from "./MyOwnCircle"; // Import the MyOwnCircle component
 import styles from "./MyOwnCircles.module.css";
 
 function MyOwnCircles() {
   const dispatch = useDispatch();
   const token = localStorage.getItem("token"); // Get user token from local storage
-  // const user = localStorage.getItem("user"); // Get user from local storage
+  const user = localStorage.getItem("user"); // Get user from local storage
 
   useEffect(() => {
     if (token) {
-      dispatch(fetchCreatedCircle(token)); // Dispatch the action to fetch study circles
+      dispatch(fetchStudyCircles(token)); // Dispatch the action to fetch study circles
     }
   }, [dispatch, token]);
 
-  const studyCircles = useSelector((state) => state.createdCircles);
+  const studyCircles = useSelector((state) => state.studyCircles);
 
-  // const myCircles = studyCircles.filter((circle) => circle.owner_name === user);
+  const myCircles = studyCircles.filter((circle) => circle.owner_name === user);
 
   return (
     <div className={styles.cardContainer}>
-      {studyCircles.map((circle) => (
+      {myCircles.map((circle) => (
         <MyOwnCircle key={circle.id} circle={circle} />
       ))}
     </div>
