2c802b973e88a7ee165a0c3ec79860a0dc1aea88
Code-Jutsu <mrimmoys@gmail.com>
2024-01-06 13:57:40 +0600

    MENTOR

--- src/Components/AdminDashboard/MentorRequest.jsx
+++ src/Components/AdminDashboard/MentorRequest.jsx
@@ -1,9 +1,17 @@
+import PropTypes from "prop-types";
+
 function MentorRequest({ mentorResponse, object_pk, id }) {
   return (
     <div>
-      <div></div>
+      <div>{(mentorResponse, object_pk, id)}</div>
     </div>
   );
 }
 
 export default MentorRequest;
+
+MentorRequest.propTypes = {
+  id: PropTypes.number.isRequired,
+  object_pk: PropTypes.number.isRequired,
+  mentorResponse: PropTypes.object.isRequired,
+};
